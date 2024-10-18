94ef25f5dff5c83fcb61a6cb0fce4297d46b28a4
Boris Obrembski <oskobri@gmail.com>
2024-10-18 13:17:09 +0200

    feat: use .env.production on build

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,27 +1,8 @@
 import axios from 'axios';
-import type {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios';
-import type {APIResponse} from "@/services/types";
-
 
 const instance = axios.create({
-    baseURL: import.meta.env.VITE_API_ENDPOINT,
+    baseURL:  import.meta.env.VITE_API_ENDPOINT,
 });
-/*
-instance.interceptors.response.use(
-    (response: AxiosResponse): APIResponse<any> => {
-        return {
-            success: true,
-            status: response.status,
-            content: response.data,
-        };
-    },
-    (error: AxiosError) => {
-        return {
-            success: false,
-            status: error.response?.status || 400,
-            content: null,
-        };
-    }
-);*/
+
 
 export default instance
